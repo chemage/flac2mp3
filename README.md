c3c19baf5eacbf5c39a4047e5f0464a443a3d869
Marcel Gerber <9885015+chemage@users.noreply.github.com>
2025-01-04 17:57:23 +0100

    converted script to for using queues

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # flac2mp3
 Convert FLAC files to MP3 recursively using a python script and ffmpeg
 
+The script uses multiprocessing to convert multiple flac files to mp3.
+It uses the system's cpu count to determine the number of processes to 
+start.
+
+The path given as command-line argument will be recursed through in search
+for flac files, which will be queued for conversion. The conversion takes
+place in the same folder structure.
+
+## Usage
+
+```shell
+python3 convert_flac2mp3.py <path>
+```
+
 ## Improvments
-- `multiprocessing` usage is not good: limit number of parallel processes and get a status of each.
-- `argparse` usage for delete is not working: correct.
+- `argparse` usage for delete is not working: to correct.
--- convert_flac2mp3.py
+++ convert_flac2mp3.py
@@ -2,54 +2,70 @@
 # file : convert_flac2mp3.py
 # convert flac to mp3 recursively and in place
 # can delete flac if required.
+# Source: https://www.digitalocean.com/community/tutorials/python-multiprocessing-example
 __author__ = "Marcel Gerber"
 __date__ = "2025-01-04"
-
 
 # system modules
 import os, sys
 import argparse
 import subprocess
-import multiprocessing
+import multiprocessing, queue, time
 
-# variables
-processes = []
 
-# classes
-
+'''
+Argument Parser
+'''
 class Help(object):
 	def __init__(self):
 		parser = argparse.ArgumentParser()
 		parser.add_argument('path', type=str, help='Path from which to start conversion')
 		parser.add_argument('-d, --delete', action='store_true', help='Delete FLAC file after conversion')
-		# parse_args defaults to [1:] for args, but you need to
-		# exclude the rest of the args too, or validation will fail
 		self.args = parser.parse_args()
 
 
-class Process(multiprocessing.Process):
-	def __init__(self, path:str, delete:bool=False): 
-		super(Process, self).__init__() 
-		self.path = path
-		self.base, self.ext = os.path.splitext(path)
-		self.delete = delete
-				 
-	def run(self):
-		print(f"Converting file '{self.path}'.")
-		# run conversion
-		subprocess.run(['ffmpeg', '-i', self.path, '-ab', '320k', '-map_metadata', '0', '-id3v2_version', '3', '-qscale:a', '0', f"{self.base}.mp3"])
-		
-		# delete source file if requested
-		if self.delete:
-			try:
-				os.remove(self.path)
-				print(f"Successfully removed '{self.path}'.")
-			except:
-				print(f"Warning: could not remove '{self.path}'. {sys.exc_info()[0]}")
+'''
+Process function which converts flac to mp3.
+Process will look for more files to be converted in the queue 
+and add them to the second queue when completed.
+- files_to_convert: Queue object of files to be converted.
+- files_converted: Queue object of iles that have been converted.
+'''
+def do_job(files_to_convert, files_converted):
+	while True:
+		try:
+			'''
+				try to get task from the queue. get_nowait() function will 
+				raise queue.Empty exception if the queue is empty. 
+				queue(False) function would do the same task also.
+			'''
+			path = files_to_convert.get_nowait()
+			base, ext = os.path.splitext(path)
+			command = ['ffmpeg', '-i', path, '-ab', '320k', '-map_metadata', '0', '-id3v2_version', '3', '-qscale:a', '0', f"{base}.mp3"]
+			sp = subprocess.run(command)
+			# print(' '.join(command))
+
+		except queue.Empty:
+			break
+
+		else:
+			'''
+				if no exception has been raised, add the task completion 
+				message to task_that_are_done queue
+			'''
+			files_converted.put(path + ' is converted by ' + multiprocessing.current_process().name)
+			time.sleep(.5)
+	
+	return True
 
 
+'''
+Build Queue by searching for flac files in folder structure.
+- path: base directory
+- delete: whether to delete flac files or not (not implemented)
+'''
 def recurse_folders(path:str, delete:bool):
-	global processes
+	global files_to_convert
 	print(f"Entering folder: '{path}'.")
 
 	for item in os.listdir(path):
@@ -57,22 +73,42 @@ def recurse_folders(path:str, delete:bool):
 		if os.path.isfile(item):
 			base, ext = os.path.splitext(item)
 			if ext.lower() == '.flac':
-				p = Process(item, delete)
-				processes += [p]
-				p.start()
-				# p.join()
+				files_to_convert.put(item)
 		elif os.path.isdir(item):
 			recurse_folders(item, delete)
 		else:
-			print(f"skip item '{item}' because it's neither a file nor a folder.")
-
+			print(f"Skip item '{item}' because it's neither a file nor a folder.")
 
 
 if __name__ == "__main__":
 	help = Help()
+	cpu_count = multiprocessing.cpu_count()
+	print("Number of cpu : ", cpu_count)
 	path = os.path.abspath(help.args.path)
 	if not os.path.isdir(path):
 		print(f"Path '{path}' is not a directory. Exiting.")
 		sys.exit(1)
 	
-	recurse_folders(path, False)
+	# initialize queues and process list
+	files_to_convert = multiprocessing.Queue()
+	files_converted = multiprocessing.Queue()
+	processes = []
+
+	# add jobs to queue
+	recurse_folders(path, delete=False)
+
+	# creating processes
+	for w in range(cpu_count):
+		p = multiprocessing.Process(target=do_job, args=(files_to_convert, files_converted))
+		processes.append(p)
+		p.start()
+
+	# completing process
+	for p in processes:
+		p.join()
+
+	# print the output
+	print("")
+	print("-- SUMMARY --")
+	while not files_converted.empty():
+		print(files_converted.get())
